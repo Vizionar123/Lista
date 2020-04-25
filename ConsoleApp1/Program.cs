53029064568661cda893ca7cb16f225c74f7bab2
Vizionar123 <62514672+Vizionar123@users.noreply.github.com>
2020-04-25 21:55:32 +0200

    Prva lista

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> lista1 = new List<int>();
+            string unos;
+            double rezultat=0;
+            do
+            {
+               
+                Console.Write("Unesite broj/Za obustavu pritisnite Enter : ");
+                unos = Console.ReadLine();
+                if (unos!="")
+                {lista1.Add(int.Parse(unos)); };
+            } while (unos != "");
+            if (lista1.Count != 0)
+            {
+                rezultat = Prosek(lista1);
+                Console.WriteLine("Prosecna vrednost unetih brojeva je : {0:#.##}", rezultat);
+            }
+            else
+            {
+                Console.WriteLine("Nije moguce napraviti proracun!!!");
+            }
+            Console.ReadKey();
+        }
+        public static double Prosek(List<int>lista1)
+        {
+           double p,s;
+            int i;
+           
+            i = 0;
+            s = 0;
+        foreach(int n in lista1)
+            {
+                s = s + n;
+                i = i + 1;//lista1.Count
+                
+            }
+            p = s / i;
+           
+
+            return (p);
+        }
+    }
+}
